96ee5514cca7baf5c7e92ff87543cc3e0ba45848
Hasan BALKANCI <swhsnblknc@gmail.com>
2022-05-17 13:50:22 +0200

    variable was added

--- linux/hands-on/linux-practice/script/shell-scriping/variable.sh
+++ linux/hands-on/linux-practice/script/shell-scriping/variable.sh
@@ -1,3 +1,11 @@
-#!/bin/bash
-NAME=Joe
-echo $NAME 
+read -p "Enter your name: " NAME
+echo "Welcome $NAME"
+
+read -s -p "Enter your password: " PASSWORD
+echo -e "\nYour password is $PASSWORD"
+
+
+
+
+
+
