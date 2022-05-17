3b216bd99b6a328a58291762e80363ad577bad9f
Hasan BALKANCI <swhsnblknc@gmail.com>
2022-05-17 13:12:21 +0200

    script file was added

---
+++ linux/hands-on/linux-practice/script/shell-scriping/basic.sh
@@ -0,0 +1,22 @@
+
+#! /bin/bash
+
+<< multiline-comment
+pwd
+ls
+Everything inside the
+HereDoc body is
+a multiline comment
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ linux/hands-on/linux-practice/script/shell-scriping/variable.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+NAME=Joe
+echo $NAME 
