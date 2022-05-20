cc1cf40e555648a98a342006ea604d6c2639538b
EC2 Default User <ec2-user@ip-172-31-43-74.eu-north-1.compute.internal>
2022-05-20 12:08:38 +0000

    i studied on script for practicing

---
+++ linux/hands-on/linux-practice/20May2022/deneme.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+working_directory=/home/ec2-user/Clarusway-AWS/linux/hands-on/linux-practice/20May2022
+echo "Welcome, your working directory is ."
+EFO
---
+++ linux/hands-on/linux-practice/20May2022/variable.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -x
+read -s -p "Enter your password: " PASSWORD
+echo -e "\nYour password is $PASSWORD"
