5cb398c42cc1bc58ddc1df65a7f5d2a4b697fc8e
Hasan BALKANCI <swhsnblknc@gmail.com>
2022-05-21 12:20:50 +0200

    function.sh  was opdated_3

--- linux/hands-on/linux-practice/linux_function/functions.sh
+++ linux/hands-on/linux-practice/linux_function/functions.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 Welcome () {
-    echo -e "Welcome to Linux Lessons \n$1 \n$2 \n$3 \n$# \n$@" 
-}
+    echo -e "Welcome to Linux Lessons \n$1 \n$2 \n$3 \n$# \n$@ \n$0" 
+    return 0
+  }
 Welcome Joe Matt Timothy
+echo $?
