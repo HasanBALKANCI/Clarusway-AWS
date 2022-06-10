17921c41b8c12842d7daa39355ee9594a73130fe
Hasan BALKANCI <swhsnblknc@gmail.com>
2022-06-10 17:39:17 +0200

    another solution of phonebook was added

--- python/coding-challenges/cc-005-create-phonebook/phone_book.py
+++ python/coding-challenges/cc-005-create-phonebook/phone_book.py
@@ -1,25 +1,38 @@
 
-namephone = {}
+def print_menu():
+    print('1. Find phone number')
+    print('2. Insert a phone number')
+    print('3. Delete a person from the phonebook')
+    print('4. Quit')
 
-num = input("Welcome to the phonebook application.\
-    Select operation on Phonebook App:\
-    1. Find phone Number\
-    2. Insert a phone number\
-    3. Delete the person from the phone_book\
-    4. Terminate")
-
-while not num == 4:
-    if num == 1:
-        name = "Insert name of the person:"
-        print("phone of", name, "is", namephone.get(name))
-        if name not in namephone.key():
-            print("Number of", name, "couldn't find.")
-    if num == 2:
-        name = "Insert :"
-        print("phone of", name, "is", namephone.get(name))
-
-
-
+numbers = {}
+menu_choice = 0
+print_menu()
+while menu_choice != 4:
+    menu_choice = int(input("Type in a number (1-4): "))
+    if menu_choice == 1:
+        x = input("The name whose phonenumber you want to learn:")
+        for x in numbers.keys():
+            print("Name: ", x, "\tNumber:", numbers[x])
+        if x not in numbers.keys():
+            print("Phone of", x, "couldn't find.")
+    elif menu_choice == 2:
+        print("Add Name and Number")
+        name = input("Name: ")
+        phone = input("Number: ")
+        numbers[name] = phone
+    elif menu_choice == 3:
+        print("Remove Name and Number")
+        name = input("Name: ")
+        if name in numbers:
+            del numbers[name]
+            print(name, "was removed succesfully.")
+        else:
+            print(name, "was not found")
+        
+    elif menu_choice != 4:
+        print_menu()
+print("Thanks to use phonebook")
     
 
     
